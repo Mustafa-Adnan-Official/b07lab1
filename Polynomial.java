c2986e5111f1c5a11d589805e8b4ec18054df8df
Mustafa Adnan <mustafamua1000@gmail.com>
2025-09-28 20:17:06 -0400

    Add some comments for clarity

--- Polynomial.java
+++ Polynomial.java
@@ -8,6 +8,8 @@ public class Polynomial {
 
     public Polynomial(double [] coeffs_map_degree) {
         //this.coeffs_map_degree = coeffs_map_degree; WRONG: aliasing not a copy
+        // aliasing: two references point to same object in mem
+        // copy: two references point to two diff objects in mem, with the same content
         if (coeffs_map_degree == null || coeffs_map_degree.length == 0) {
             this.coeffs_map_degree = new double[1];
             this.coeffs_map_degree[0] = 0.0;
@@ -23,12 +25,13 @@ public class Polynomial {
     public Polynomial add (Polynomial p) {
         int highest_deg = Math.max(this.coeffs_map_degree.length, p.coeffs_map_degree.length);
         int lower_deg = Math.min(this.coeffs_map_degree.length, p.coeffs_map_degree.length);
-        double [] new_coeffs = new double[highest_deg];
+        double [] new_coeffs = new double[highest_deg]; // new arr with sufficient size for highest degree (init. to 0.0)
         
+        // add coeffs for degrees that both polys have
         for (int i=0; i<lower_deg; i++){
             new_coeffs[i] = this.coeffs_map_degree[i] + p.coeffs_map_degree[i];
         }
-
+        // copy remaining coeffs from the poly with the higher degree
         if (this.coeffs_map_degree.length > p.coeffs_map_degree.length) {
             for (int i=lower_deg; i<highest_deg; i++){
                 new_coeffs[i] = this.coeffs_map_degree[i];
